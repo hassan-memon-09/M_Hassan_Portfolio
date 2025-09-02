fba1a3c2cd9ebbf61267d21cd18d7d616ed06802
hassan-memon-09 <muhammadhassan.pitp@gmail.com>
2025-09-02 20:13:06 +0500

    Setup

---
+++ portfolio/src/components/Footer.jsx
@@ -0,0 +1,9 @@
+const Footer = () => {
+  return (
+    <footer className="py-4 bg-slate-900 text-center">
+      <p>&copy; 2025 YourName. All rights reserved.</p>
+    </footer>
+  );
+};
+
+export default Footer;
